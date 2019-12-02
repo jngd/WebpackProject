ccc1c10600b9e74f19d8cd360b6c40a462d39129
jngd <juangrdiaz@gmail.com>
2019-12-02 20:01:32 +0100

    tweak production deployment

--- webpack.config.js
+++ webpack.config.js
@@ -37,5 +37,8 @@ module.exports = {
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({ names: ['vendor', 'manifest'] }),
     new HtmlWebpackPlugin({ template: 'src/index.html' }),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
   ],
 };
